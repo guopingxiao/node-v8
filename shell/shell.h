c06e795e67024dbe7d766662041f10807e9babaa
xiaoguoping <xiaoguoping@meituan.com>
2020-02-09 18:48:45 +0800

    update

---
+++ shell/shell.h
@@ -0,0 +1,16 @@
+
+#ifndef V8DEMO_SHELL_H
+#define V8DEMO_SHELL_H
+
+class Point {
+public:
+  Point(int x, int y) : x_(x), y_(y) { }
+  int x_, y_;
+
+
+  int multi() {
+    return this->x_ * this->y_;
+  }
+};
+
+#endif //V8DEMO_SHELL_H
